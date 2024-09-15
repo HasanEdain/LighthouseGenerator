55b595327a969955623efc1cbbe7e19055f96ddc
Hasan Edain <hasane@npcunlimited.com>
2024-09-15 14:16:37 -0700

    Initial Commit

---
+++ LighthouseGenerator/LighthouseGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LighthouseGeneratorApp.swift
+//  LighthouseGenerator
+//
+//  Created by Hasan Edain on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct LighthouseGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
